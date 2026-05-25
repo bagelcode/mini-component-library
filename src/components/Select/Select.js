6f60d6108a8107dd08a05692690dc835ea5cc823
Adam B <abacigal92@gmail.com>
2026-05-25 13:55:19 -0400

    Select Box done

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,60 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
+    <SelectWrapper>
+    <StyledSelect value={value} onChange={onChange}>
       {children}
-    </select>
+      
+    </StyledSelect>
+    <Icon id='chevron-down' style={{'position':'absolute', 'top':50+'%', 'transform': 'translateY(-50%)', 'right': 16+'px', 'pointer-events': 'none'}}/>
+    </SelectWrapper>
   );
 };
 
+
+const SelectWrapper = styled.div`
+  position: relative;
+  background-color: ${COLORS.transparentGray15};
+  padding: 16px;
+  border-radius: 8px;
+  width: fit-content;
+
+
+  &:focus-visible{
+  border: 2px solid blue;
+  }
+  
+
+`
+
+const StyledSelect = styled.select`
+appearance: none;
+position: relative;
+font-size: 1rem;
+color: ${COLORS.gray700};
+border: none;
+background-color: transparent;
+width: fit-content;
+padding-right: 32px;
+
+
+&:hover {
+color: ${COLORS.black}}
+
+
+}
+
+
+`
+
+
+
+
+
+
+
 export default Select;
+
+
+/* this is how I solved it. Josh did a different way where he put a presentational layer over top. */
+
